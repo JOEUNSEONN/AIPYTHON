3a7912dfe7ca8613cb27e9e5ef5f066721a38000
조은선 <choes611@gmail.com>
2025-03-31 01:42:05 +0000

    구구단

---
+++ test.py
@@ -0,0 +1,12 @@
+# number = 4  # 숫자를 4로 고정
+s_number = "4"  # 사용자 입력 대신 4를 문자열로 저장
+print(f"{s_number}단:")
+# print(number, "단:")
+# print(number + "단:")
+# print(number + "단, " + number + "단, " + number + "단")
+# print(f"{number}단, {number}단, {number}단")
+
+# number 값을 정수로 변환 후 연산
+number = int(s_number)
+result = number * 4
+print(result)
